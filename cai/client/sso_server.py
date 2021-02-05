9e7850206df8e86bf8a34d56b37671c9e6dfd8ff
yanyongyu <yanyongyu_1@126.com>
2021-02-05 16:25:42 +0800

    :bulb: add sso server doc string

--- cai/client/sso_server.py
+++ cai/client/sso_server.py
@@ -29,6 +29,14 @@ _cached_servers: List["SsoServer"] = []
 
 
 class _FakeSocket:
+    """Fake socket object.
+
+    This class is used to wrap raw response bytes
+    and pass them to :class:`http.client.HTTPResponse`.
+
+    Args:
+        response (:obj:`bytes`): HTTP raw response from server.
+    """
 
     def __init__(self, response: bytes):
         self._file = BytesIO(response)
@@ -37,93 +45,160 @@ class _FakeSocket:
         return self._file
 
 
-# renamed from: com.tencent.msf.service.protocol.serverconfig.d
 class SsoServerRequest(JceStruct):
+    """Sso Server List Request.
+
+    Note:
+        Source: com.tencent.msf.service.protocol.serverconfig.d
+    """
     uin: types.INT64 = JceField(0, jce_id=1)  # uin or 0
-    timeout: types.INT64 = JceField(
-        0, jce_id=2
-    )  # may be timeout (s), default is 60
+    """:obj:`jce.types.INT64`: User QQ or 0, renamed from: a."""
+    timeout: types.INT64 = JceField(0, jce_id=2)
+    """:obj:`jce.types.INT64`: may be timeout (s), default is 60, renamed from: b."""
+    c: types.BYTE = JceField(bytes([1]), jce_id=3)
+    """:obj:`jce.types.BYTE`: always 1."""
+    imsi: types.STRING = JceField("46000", jce_id=4)
+    """:obj:`jce.types.STRING`: imsi string, renamed from: d.
 
-    f172842c: types.BYTE = JceField(bytes([1]), jce_id=3)  # always 1
-    imsi: types.STRING = JceField(
-        "46000", jce_id=4
-    )  # imsi: null->'' or imsi.substring(0, 5)
-    is_wifi: types.INT32 = JceField(
-        100, jce_id=5
-    )  # NetConnInfoCenter.isWifiConn: true->100, false->1
-    app_id: types.INT64 = JceField(
-        jce_id=6
-    )  # cai.settings.protocol.ApkInfo.app_id
-    imei: types.STRING = JceField(jce_id=7)  # imei
-    cell_id: types.INT64 = JceField(
-        0, jce_id=8
-    )  # CdmaCellLocation.getBaseStationId
-    f172848i: types.INT64 = JceField(0, jce_id=9)
-    f172849j: types.INT64 = JceField(0, jce_id=10)
-    f172850k: types.BYTE = JceField(
-        bytes(1), jce_id=11
-    )  # Unknown bool(false): true->1, false->0
-    f172851l: types.BYTE = JceField(
-        bytes(1), jce_id=12
-    )  # NetConnInfoCenter.getActiveNetIpFamily
-    f172852m: types.INT64 = JceField(0, jce_id=13)
+    ``null``->``""`` or ``imsi.substring(0, 5)``.
+    """
+    is_wifi: types.INT32 = JceField(100, jce_id=5)
+    """:obj:`jce.types.INT32`: is_wifi, renamed from: e.
+
+    NetConnInfoCenter.isWifiConn: true->100, false->1.
+    """
+    app_id: types.INT64 = JceField(jce_id=6)
+    """:obj:`jce.types.INT64`: app_id, renamed from: f.
+
+    same to :attr:`cai.settings.protocol.ApkInfo.app_id`.
+    """
+    imei: types.STRING = JceField(jce_id=7)
+    """:obj:`jce.types.STRING`: imei, renamed from: g.
+
+    same to :attr:`cai.settings.device.DeviceInfo.imei`.
+    """
+    cell_id: types.INT64 = JceField(0, jce_id=8)
+    """:obj:`jce.types.INT64`: cell_id, renamed from: h.
+
+    cell location get from ``CdmaCellLocation.getBaseStationId``.
+    """
+    i: types.INT64 = JceField(0, jce_id=9)
+    """:obj:`jce.types.INT64`: unknown."""
+    j: types.INT64 = JceField(0, jce_id=10)
+    """:obj:`jce.types.INT64`: unknown."""
+    k: types.BYTE = JceField(bytes(1), jce_id=11)
+    """:obj:`jce.types.BYTE`: unknown boolean, true->1, false->0."""
+    l: types.BYTE = JceField(bytes(1), jce_id=12)
+    """:obj:`jce.types.BYTE`: active net ip family.
+
+    get from ``NetConnInfoCenter.getActiveNetIpFamily``.
+    """
+    m: types.INT64 = JceField(0, jce_id=13)
+    """:obj:`jce.types.INT64`: unknown."""
 
 
-# renamed from: com.tencent.msf.service.protocol.serverconfig.i
 class SsoServer(JceStruct):
+    """Sso Server Info.
+
+    Note:
+        Source: com.tencent.msf.service.protocol.serverconfig.i
+    """
     host: types.STRING = JceField(jce_id=1)
+    """:obj:`jce.types.STRING`: server host, renamed from: a."""
     port: types.INT32 = JceField(jce_id=2)
-    # f172901c: types.BYTE = JceField(jce_id=3)
-    # f172902d: types.BYTE = JceField(jce_id=4)
-    protocol: types.BYTE = JceField(jce_id=5)  # 0,1: socket; 2,3: http
-    # f172904f: types.INT32 = JceField(jce_id=6)
-    # f172905g: types.BYTE = JceField(jce_id=7)
+    """:obj:`jce.types.INT32`: server port, renamed from: b."""
+    # c: types.BYTE = JceField(jce_id=3)
+    # d: types.BYTE = JceField(jce_id=4)
+    protocol: types.BYTE = JceField(jce_id=5)
+    """:obj:`jce.types.BYTE`: protocol, renamed from: e.
+
+    0, 1: socket; 2, 3: http.
+    """
+    # f: types.INT32 = JceField(jce_id=6)
+    # g: types.BYTE = JceField(jce_id=7)
     city: types.STRING = JceField(jce_id=8)
+    """:obj:`jce.types.STRING`: city, renamed from: h."""
     country: types.STRING = JceField(jce_id=9)
+    """:obj:`jce.types.STRING`: country, renamed from: i."""
 
 
-# renamed from: com.tencent.msf.service.protocol.serverconfig.e
 class SsoServerResponse(JceStruct):
-    # f172861a: types.INT32 = JceField(0, jce_id=1)
-    socket_v4_mobile: types.LIST[SsoServer] = JceField(
-        jce_id=2
-    )  # socket, ipv4, mobile
-    socket_v4_wifi: types.LIST[SsoServer] = JceField(
-        jce_id=3
-    )  # socket, ipv4, wifi
-    # f172864d: types.INT32 = JceField(0, jce_id=4)
-    # f172865e: types.INT32 = JceField(86400, jce_id=5)
-    # f172866f: types.BYTE = JceField(bytes(1), jce_id=6)
-    # f172867g: types.BYTE = JceField(bytes(1), jce_id=7)
-    # f172868h: types.INT32 = JceField(1, jce_id=8)
-    # f172869i: types.INT32 = JceField(5, jce_id=9)
-    # f172870j: types.INT64 = JceField(0, jce_id=10)
-    # f172871k: types.INT32 = JceField(0, jce_id=11)
-    http_v4_mobile: types.LIST[SsoServer] = JceField(
-        jce_id=12
-    )  # http, ipv4, mobile
-    http_v4_wifi: types.LIST[SsoServer] = JceField(
-        jce_id=13
-    )  # http, ipv4, wifi
-    speed_info: types.BYTES = JceField(
-        jce_id=14
-    )  # vCesuInfo, PacketVersion3(QualityTest)
-    socket_v6: types.LIST[SsoServer] = JceField(
-        jce_id=15
-    )  # socket, ipv6, (wifi and nettype & 1 == 1) | (mobile and nettype & 2 == 2)
-    http_v6: types.LIST[SsoServer] = JceField(
-        jce_id=16
-    )  # http, ipv6, (wifi and nettype & 1 == 1) | (mobile and nettype & 2 == 2)
-    udp_v6: types.LIST[SsoServer] = JceField(
-        jce_id=17
-    )  # quic, ipv6, (wifi and nettype & 1 == 1) | (mobile and nettype & 2 == 2)
+    """Sso Server List Response.
+
+    Note:
+        Source: com.tencent.msf.service.protocol.serverconfig.e
+    """
+    # a: types.INT32 = JceField(0, jce_id=1)
+    socket_v4_mobile: types.LIST[SsoServer] = JceField(jce_id=2)
+    """:obj:`jce.types.LIST` of :obj:`.SsoServer`:
+    socket ipv4 mobile server, renamed from: b.
+    """
+    socket_v4_wifi: types.LIST[SsoServer] = JceField(jce_id=3)
+    """:obj:`jce.types.LIST` of :obj:`.SsoServer`:
+    socket ipv4 wifi server, renamed from: c.
+    """
+    # d: types.INT32 = JceField(0, jce_id=4)
+    # e: types.INT32 = JceField(86400, jce_id=5)
+    # f: types.BYTE = JceField(bytes(1), jce_id=6)
+    # g: types.BYTE = JceField(bytes(1), jce_id=7)
+    # h: types.INT32 = JceField(1, jce_id=8)
+    # i: types.INT32 = JceField(5, jce_id=9)
+    # j: types.INT64 = JceField(0, jce_id=10)
+    # k: types.INT32 = JceField(0, jce_id=11)
+    http_v4_mobile: types.LIST[SsoServer] = JceField(jce_id=12)
+    """:obj:`jce.types.LIST` of :obj:`.SsoServer`:
+    http ipv4 mobile server, renamed from: l.
+    """
+    http_v4_wifi: types.LIST[SsoServer] = JceField(jce_id=13)
+    """:obj:`jce.types.LIST` of :obj:`.SsoServer`:
+    http ipv4 wifi server, renamed from: m.
+    """
+    speed_info: types.BYTES = JceField(jce_id=14)
+    """:obj:`jce.types.BYTES`: vCesuInfo, renamed from: n.
+
+    bytes from :class:`~cai.utils.jce.RequestPacketVersion3`(QualityTest)
+    """
+    socket_v6: types.LIST[SsoServer] = JceField(jce_id=15)
+    """:obj:`jce.types.LIST` of :obj:`.SsoServer`:
+    socket ipv6 server, renamed from: o.
+
+    used when ``wifi and :attr:`.SsoServerResponse.nettype` & 1 == 1``
+    or ``(mobile and :attr:`.SsoServerResponse.nettype` & 2 == 2)``
+    """
+    http_v6: types.LIST[SsoServer] = JceField(jce_id=16)
+    """:obj:`jce.types.LIST` of :obj:`.SsoServer`:
+    http ipv6 server, renamed from: p.
+
+    used when ``wifi and :attr:`.SsoServerResponse.nettype` & 1 == 1``
+    or ``(mobile and :attr:`.SsoServerResponse.nettype` & 2 == 2)``
+    """
+    udp_v6: types.LIST[SsoServer] = JceField(jce_id=17)
+    """:obj:`jce.types.LIST` of :obj:`.SsoServer`:
+    quic ipv6 server, renamed from: q.
+
+    used when ``wifi and :attr:`.SsoServerResponse.nettype` & 1 == 1``
+    or ``(mobile and :attr:`.SsoServerResponse.nettype` & 2 == 2)``
+    """
     nettype: types.BYTE = JceField(bytes(1), jce_id=18)
+    """:obj:`jce.types.BYTE`: nettype, renamed from: r."""
     delay_threshold: types.INT32 = JceField(0, jce_id=19)
+    """:obj:`jce.types.INT32`: delay threshold, renamed from: s."""
     policy_id: types.STRING = JceField("", jce_id=20)
+    """:obj:`jce.types.STRING`: policy id, renamed from: t."""
 
 
-# com/tencent/mobileqq/msf/core/p205a/C25979f.java
 async def get_sso_list() -> SsoServerResponse:
+    """Do sso server list request and return the response.
+
+    Note:
+        Source: com.tencent.mobileqq.msf.core.a.f.run
+
+    Returns:
+        SsoServerResponse: Sso server list response
+
+    Raises:
+        SsoServerException: Get sso server list failed.
+    """
     device = get_device()
     protocol = get_protocol()
     key = bytes(
@@ -180,6 +255,16 @@ async def quality_test(
     servers: Iterable[SsoServer],
     threshold: float = 500.
 ) -> List[Tuple[SsoServer, float]]:
+    """Test given servers' quality by tcp latency.
+
+    Args:
+        servers (Iterable[:obj:`.SsoServer`]): Server list to test.
+        threshold (float, optional): Latency more than threshold will not be returned.
+            Defaults to 500.0.
+
+    Returns:
+        List[Tuple[:obj:`.SsoServer`, float]]: List of server and latency in tuple.
+    """
     tasks = [tcp_latency_test(server.host, server.port) for server in servers]
     result: List[Union[float, Exception]
                 ] = await asyncio.gather(*tasks, return_exceptions=True)
@@ -192,6 +277,14 @@ async def quality_test(
 
 
 async def get_sso_server(cache: bool = True) -> SsoServer:
+    """Get the best sso server
+
+    Args:
+        cache (bool, optional): Using cache or not. Defaults to True.
+
+    Returns:
+        :obj:`.SsoServer`: The best server with smallest latency.
+    """
     global _cached_server
     if cache and _cached_server:
         return _cached_server

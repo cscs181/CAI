b7b376834d10be8aaa813f8ae03b469de70c4e06
a1025 <a1025952421@outlook.com>
2022-03-20 18:10:36 +0800

    fix: some bugs

--- cai/client/multi_msg/long_msg.py
+++ cai/client/multi_msg/long_msg.py
@@ -1,5 +1,5 @@
 from cai.client import Command
-from cai.pb.highway.longmsg.longmsg_pb2 import MultiReqBody, MultiMsgApplyUpReq, MultiMsgApplyUpRsp
+from cai.pb.highway.longmsg.longmsg_pb2 import MultiReqBody, MultiMsgApplyUpReq, MultiMsgApplyUpRsp, MultiRspBody
 from typing import TYPE_CHECKING
 
 from dataclasses import dataclass
@@ -9,7 +9,7 @@ if TYPE_CHECKING:
     from cai.client.client import Client
 
 
-def encode_multi_apply_up_pkg(group_id: int, data_len: int, data_md5: bytes, bu_type: int) -> bytes:
+def _encode_multi_req_body(group_id: int, data_len: int, data_md5: bytes, bu_type: int) -> MultiReqBody:
     return MultiReqBody(
         subcmd=1,
         termType=5,
@@ -23,19 +23,33 @@ def encode_multi_apply_up_pkg(group_id: int, data_len: int, data_md5: bytes, bu_
             msgType=3
         )],
         buType=bu_type
-    ).SerializeToString()
+    )
 
 
 @dataclass
-class MultiApplyUpResp(Command):
-    data: MultiMsgApplyUpRsp
+class MultiApplyResp(Command):
+    data: MultiRspBody
 
 
-async def handle_multi_apply_up_resp(client: "Client", pkg: "IncomingPacket", _device):
-    return MultiApplyUpResp(
+async def build_multi_apply_up_pkg(client: "Client", group_id: int, data_len: int, data_md5: bytes, bu_type: int):
+    body: MultiApplyResp = await client.send_unipkg_and_wait(
+        client.next_seq(),
+        "MultiMsg.ApplyUp",
+        _encode_multi_req_body(
+            group_id, data_len, data_md5, bu_type
+        ).SerializeToString()
+    )
+    
+
+
+
+
+
+async def handle_multi_resp_body(client: "Client", pkg: "IncomingPacket", _device) -> MultiApplyResp:
+    return MultiApplyResp(
         uin=pkg.uin,
         seq=pkg.seq,
         ret_code=pkg.ret_code,
         command_name=pkg.command_name,
-        data=MultiMsgApplyUpRsp.FromString(pkg.data)
+        data=MultiRspBody.FromString(pkg.data)
     )
